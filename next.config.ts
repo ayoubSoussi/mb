73ecfe0747bf1a61d124c7741105738fc00aa884
ayoubPerso <34512229+ayoubSoussi@users.noreply.github.com>
2026-02-13 11:23:45 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/maryam-bakkar',
-  assetPrefix: '/maryam-bakkar',
+  assetPrefix: '/mb',
   images: {
     unoptimized: true
   },
